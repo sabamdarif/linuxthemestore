a58ef16466dda097d1a76fbf17513fa26fdc04de
Debasish Patra <patradebasish1987@gmail.com>
2025-05-28 10:02:29 +0530

    ..

--- prepare_flatpak.sh
+++ prepare_flatpak.sh
@@ -1,7 +1,13 @@
 sudo rm -R .flatpak-builder
+cargo vendor vendor
 flatpak-builder --force-clean build/ ./com.github.debasishpatra1987.linuxthemestore.json 
 flatpak build-export export build
 flatpak build-bundle export com.github.debasishpatra1987.linuxthemestore.flatpak com.github.debasishpatra1987.linuxthemestore 
 flatpak install --user com.github.debasishpatra1987.linuxthemestore.flatpak -y
 flatpak run com.github.debasishpatra1987.linuxthemestore
+
+flatpak uninstall --user com.github.debasishpatra1987.linuxthemestore -y
+rm -R vendor
+rm -R build
+
 
